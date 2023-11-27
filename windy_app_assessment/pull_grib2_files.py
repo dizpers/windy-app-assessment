e1062364613b62739069e8b30ef68b6d5b06f2ab
dizpers <dizpers@gmail.com>
2023-11-27 23:43:38 +0700

    Pass Path objects instead of a string

--- windy_app_assessment/pull_grib2_files.py
+++ windy_app_assessment/pull_grib2_files.py
@@ -20,25 +20,36 @@ RESULT_DIR_PATH = Path(__file__).parent.parent.absolute() / "result"
 ICON_D2_DIR_PATH = RESULT_DIR_PATH / "icon_d2"
 
 
-def get_wgf4_output_dir(grib2_file_path: str) -> str:
-    year, month, day, hour, offset = map(int, re.match(r".*(\d{4})(\d{2})(\d{2})(\d{2})_(\d{3})_2d", grib2_file_path).groups())
+def get_wgf4_output_dir_name(grib2_file_path: Path) -> str:
+    """
+    The result of GRIB2 files processing is the specific directory structure and PRATE.wgf4 files inside. This function
+    generates the directory name for a given GRIB2 file.
+
+    :param grib2_file_path: Path object representing a path to given GRIB2 file
+    :return: a directory name where WGF4 file would be created
+    """
+    year, month, day, hour, offset = map(
+        int,
+        re.match(r".*(\d{4})(\d{2})(\d{2})(\d{2})_(\d{3})_2d", str(grib2_file_path)).groups()
+    )
+    # TODO (dmitry): have a constant for date/time format
     return (datetime(year, month, day, hour) + timedelta(hours=offset)).strftime("%d.%m.%Y_%H:%M_%s")
 
 
 
-async def process_grib2_files(grib2_files_directory: str) -> None:
-    for grib2_file_path in sorted(Path(grib2_files_directory).iterdir()):
+async def process_grib2_files(grib2_files_directory_path: Path) -> None:
+    for grib2_file_path in sorted(grib2_files_directory_path.iterdir()):
         # create directory for current hour
         # TODO (dmitry): if a directory exists, we might already have the data there, so ideally we shouldn't prcess
         # current GRIB2 file in this case
         # but here we'll just ignore that directory exists, the new WGF4 file would be created and stored in this dir
-        (ICON_D2_DIR_PATH / get_wgf4_output_dir(str(grib2_file_path))).mkdir(exist_ok=True)
+        (ICON_D2_DIR_PATH / get_wgf4_output_dir_name(grib2_file_path)).mkdir(exist_ok=True)
         # prepare PRATE.wgf4 header
 
 
 async def download_grib2_file(
         session: aiohttp.ClientSession,
-        grib2_files_directory: str,
+        grib2_files_directory_path: Path,
         grib2_file_url: str
 ) -> None:
     """
@@ -49,7 +60,7 @@ async def download_grib2_file(
     * all the files would be saved to temporary directory
 
     :param session: aiohttp session
-    :param grib2_files_directory: temporary directory for GRIB2 files
+    :param grib2_files_directory_path: Path object representing a path to temporary directory with GRIB2 files
     :param grib2_file_url: URL to (bzip2 compressed) GRIB2 file
     """
     async with session.get(grib2_file_url) as resp:
@@ -57,7 +68,7 @@ async def download_grib2_file(
         if resp.status == 200:
             file_name = grib2_file_url.split("/")[-1][:-4]
             # TODO (dmitry): consider in-memory files to reduce time spent on disk i/o operations
-            async with aiofiles.open(Path(grib2_files_directory) / file_name, "wb") as decompressed_file:
+            async with aiofiles.open(grib2_files_directory_path / file_name, "wb") as decompressed_file:
                 decompressor = BZ2Decompressor()
                 async for data in resp.content.iter_any():
                     await decompressed_file.write(decompressor.decompress(data))
@@ -87,13 +98,17 @@ async def get_grib2_files_urls(session: aiohttp.ClientSession, grib2_files_direc
 
 
 async def async_main():
+    # TODO (dmitry): replace print statements with logging
+    print("Starting")
+
     async with aiohttp.ClientSession() as session:
         async with aiofiles.tempfile.TemporaryDirectory() as grib2_files_directory:
+            grib2_files_directory_path: Path = Path(grib2_files_directory)
             await asyncio.gather(*([
-                download_grib2_file(session, grib2_files_directory, grib2_file_url)
+                download_grib2_file(session, grib2_files_directory_path, grib2_file_url)
                 for grib2_file_url in await get_grib2_files_urls(session, GRIB2_FILES_DIRECTORY_URL)
             ]))
-            await process_grib2_files(grib2_files_directory)
+            await process_grib2_files(grib2_files_directory_path)
 
     print("All done")
 
